3a31f998766104cce5d0385bac72b5ce373d88b3
Urko Lekuona <urko@arima.eu>
2021-04-15 15:53:29 +0200

    LoggerListener original

--- src/main/java/eu/arima/LoggerListener.java
+++ src/main/java/eu/arima/LoggerListener.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2004, 2005, 2006 Acegi Technology Pty Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package eu.arima;
 
 import org.apache.commons.logging.Log;
@@ -10,10 +26,17 @@ import org.springframework.util.ClassUtils;
 import org.springframework.security.authentication.event.*;
 import org.springframework.stereotype.Component;
 
+/**
+ * Outputs authentication-related application events to Commons Logging.
+ * <p>
+ * All authentication events are logged at the warning level.
+ *
+ * @author Ben Alex
+ */
 @Component
-public class LogListener implements ApplicationListener<AbstractAuthenticationEvent> {
+public class LoggerListener implements ApplicationListener<AbstractAuthenticationEvent> {
 
-	private static final Log logger = LogFactory.getLog(LogListener.class);
+	private static final Log logger = LogFactory.getLog(LoggerListener.class);
 
 	/**
 	 * If set to true, {@link InteractiveAuthenticationSuccessEvent} will be logged
