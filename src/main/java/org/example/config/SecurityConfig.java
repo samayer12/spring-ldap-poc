acf61c713ef3c8172f86cf5d19e025f102a9468d
Sam Mayer <samuel.a.mayer5.ctr@army.mil>
2023-04-27 14:49:05 -0500

    Refactor LDAP Authentication to use spring-security-ldap:6.0.2

--- src/main/java/org/example/config/SecurityConfig.java
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -3,23 +3,20 @@ package org.example.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.ldap.core.support.BaseLdapPathContextSource;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
+import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
 import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 import org.springframework.security.ldap.userdetails.DefaultLdapAuthoritiesPopulator;
 import org.springframework.security.ldap.userdetails.LdapAuthoritiesPopulator;
-
-import java.util.Set;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * Security Configuration - LDAP and HTTP Authorizations.
  */
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     @Value("${spring.ldap.urls:ldap://localhost:1389}")
     private String ldapUrl;
@@ -27,56 +24,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Value("${spring.ldap.base:dc=example,dc=org}")
     private String baseDn;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.ldapAuthentication()
-                .contextSource(contextSource())
-                .userSearchFilter("(uid={0})")
-                .userSearchBase("ou=people")
-                .ldapAuthoritiesPopulator(ldapAuthoritiesPopulator())
-                .groupSearchFilter("(member={0})")
-                .groupSearchBase("ou=groups")
-                .passwordCompare()
-                    .passwordEncoder(new LdapShaPasswordEncoder())
-                    .passwordAttribute("userPassword")
-        ;
-    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authz) -> authz
+          .requestMatchers("/").permitAll()
+          .anyRequest().authenticated())
+          .formLogin().permitAll().and().logout().logoutSuccessUrl("/");
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/").permitAll()
-                .anyRequest().authenticated().and()
-                .formLogin().permitAll().and().logout().logoutSuccessUrl("/");
+        return http.build();
     }
 
     @Bean
-    public LdapAuthoritiesPopulator ldapAuthoritiesPopulator() {
-        DefaultLdapAuthoritiesPopulator populi = new DefaultLdapAuthoritiesPopulator(contextSource(), "ou=groups") {
+    protected LdapAuthoritiesPopulator ldapAuthoritiesPopulator(BaseLdapPathContextSource contextSource) {
+        String groupSearchBase = "admin";
+        DefaultLdapAuthoritiesPopulator authorities =
+          new DefaultLdapAuthoritiesPopulator(contextSource, groupSearchBase);
+        authorities.setGroupSearchFilter("member={0}");
 
-            private static final String ADMIN_ROLE = "ROLE_ADMIN";
-            @Override
-            public Set<GrantedAuthority> getGroupMembershipRoles(String userDn, String username) {
-                Set<GrantedAuthority> groupMembershipRoles = super.getGroupMembershipRoles(userDn, username);
+        return authorities;
+    }
 
-                boolean isMemberOfSpecificAdGroup = false;
-                for (GrantedAuthority grantedAuthority : groupMembershipRoles) {
-
-                    if (ADMIN_ROLE.equals(grantedAuthority.toString())) {
-                        isMemberOfSpecificAdGroup = true;
-                        break;
-                    }
-                }
-
-                if (!isMemberOfSpecificAdGroup) {
-
-                    throw new BadCredentialsException("User must be a member of " + ADMIN_ROLE);
-                }
-                return groupMembershipRoles;
-            }
-        };
-
-        return populi;
+    @Bean
+    protected AuthenticationManager authenticationManager(BaseLdapPathContextSource contextSource, LdapAuthoritiesPopulator authorities) {
+        LdapBindAuthenticationManagerFactory factory = new LdapBindAuthenticationManagerFactory(contextSource);
+        factory.setUserSearchFilter("(uid={0})");
+        factory.setUserSearchBase("ou=people");
+        return factory.createAuthenticationManager();
     }
 
     @Bean
