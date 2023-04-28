a543be376da405417f645668adb0c1ea9555e573
Sam Mayer <samuel.a.mayer5.ctr@army.mil>
2023-04-28 09:34:46 -0500

    Authenticate, but don't authorize, requests

--- src/main/java/org/example/config/SecurityConfig.java
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authz) -> authz
+        http.authorizeRequests((authz) -> authz
           .antMatchers("/").permitAll()
           .anyRequest().authenticated())
           .formLogin().permitAll().and().logout().logoutSuccessUrl("/");
