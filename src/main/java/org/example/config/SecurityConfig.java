6deea476c71afe96ca3267317542abcf046ef816
Sam Mayer <samuel.a.mayer5.ctr@army.mil>
2023-04-28 11:28:54 -0500

    Apply LdapAuthoritiesPopulator to LdapBindAuthenticationManagerFactory

--- src/main/java/org/example/config/SecurityConfig.java
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -5,13 +5,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.ldap.core.support.BaseLdapPathContextSource;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 import org.springframework.security.ldap.userdetails.DefaultLdapAuthoritiesPopulator;
 import org.springframework.security.ldap.userdetails.LdapAuthoritiesPopulator;
 import org.springframework.security.ldap.userdetails.PersonContextMapper;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Set;
 
 /**
  * Security Configuration - LDAP and HTTP Authorizations.
@@ -29,6 +33,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests((authz) -> authz
           .antMatchers("/").permitAll()
+          .antMatchers("/secure").authenticated()
           .anyRequest().authenticated())
           .formLogin().permitAll().and().logout().logoutSuccessUrl("/");
 
@@ -38,11 +43,32 @@ public class SecurityConfig {
     @Bean
     protected LdapAuthoritiesPopulator ldapAuthoritiesPopulator(BaseLdapPathContextSource contextSource) {
         String groupSearchBase = "";
-        DefaultLdapAuthoritiesPopulator authorities =
-          new DefaultLdapAuthoritiesPopulator(contextSource, groupSearchBase);
-        authorities.setGroupSearchFilter("member={0}");
 
-        return authorities;
+           DefaultLdapAuthoritiesPopulator populi = new DefaultLdapAuthoritiesPopulator(contextSource(), "ou=groups") {
+
+            private static final String ADMIN_ROLE = "ROLE_ADMIN";
+            @Override
+            public Set<GrantedAuthority> getGroupMembershipRoles(String userDn, String username) {
+                Set<GrantedAuthority> groupMembershipRoles = super.getGroupMembershipRoles(userDn, username);
+
+                boolean isMemberOfSpecificAdGroup = false;
+                for (GrantedAuthority grantedAuthority : groupMembershipRoles) {
+
+                    if (ADMIN_ROLE.equals(grantedAuthority.toString())) {
+                        isMemberOfSpecificAdGroup = true;
+                        break;
+                    }
+                }
+
+                if (!isMemberOfSpecificAdGroup) {
+
+                    throw new BadCredentialsException("User must be a member of " + ADMIN_ROLE);
+                }
+                return groupMembershipRoles;
+            }
+        };
+
+        return populi;
     }
 
     @Bean
@@ -51,6 +77,7 @@ public class SecurityConfig {
         factory.setUserSearchFilter("(uid={0})");
         factory.setUserSearchBase("ou=people");
         factory.setUserDetailsContextMapper(new PersonContextMapper());
+        factory.setLdapAuthoritiesPopulator(ldapAuthoritiesPopulator(contextSource));
         return factory.createAuthenticationManager();
     }
 
